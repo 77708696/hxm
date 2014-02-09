b30e03c94b995b96e5001d1da2efb88ba2ec6c65
77708696 <77708696@qq.com>
2014-02-09 15:01:28 +0800

    修正乱码

--- src/com/git/Tmain.java
+++ src/com/git/Tmain.java
@@ -5,8 +5,12 @@ public class Tmain {
 	public static void main(String[] args) {
 
 		System.out.println("测试GIT");
+		
+		System.out.println("测试GIT");
+		
+		System.out.println("测试GIT");
 		/*
-		 * 
+		 * 添加了说明
 		 * 
 		 * 
 		 * asfasdfad*/
