d9141e52f3d7f40036c436b184da6b481772b9c4
77708696 <77708696@qq.com>
2014-02-09 17:24:47 +0800

    完成读取配置文件

--- src/com/git/Tmain.java
+++ src/com/git/Tmain.java
@@ -1,22 +1,30 @@
 package com.git;
 
+import java.io.File;
+/**
+ * 
+ * @author 黄小明2014年2月9日
+ *
+ */
 public class Tmain {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("测试GIT");
 		
-		System.out.println("测试GIT");
+		String s="";
+		//Thread.currentThread().
 		
-		System.out.println("测试GIT");
-		/*
-		 * 添加了说明
-		 * 
-		 * 
-		 * asfasdfad*/
+		System.out.println(System.getProperty("user.dir"));
+		config cf = new config(System.getProperty("user.dir") + File.separator + "a.txt");
+		System.out.println(cf.GetValue("IcisReport.server.ip"));
 		
+		cf.listall();
 		
-
 	}
 
 }
--- src/com/git/config.java
+++ src/com/git/config.java
@@ -1,7 +1,48 @@
 package com.git;
-
+import java.util.*;
+import java.io.*;
+/**
+ * 
+ * @author Administrator
+ *
+ */
 public class config {
 	private String dbuser="root";
 	private String dbpwd="123456";
 	private String IP="192.168.1.1";
+	private Properties props = new Properties();
+	public config(){	
+	}
+	/**
+	 * 加载配置文件
+	 * 
+	 * */
+	public config(String filePath){
+		 try {
+	         InputStream in = new BufferedInputStream (new FileInputStream(filePath));
+	         props.load(in);
+		 }catch(IOException e){
+			 
+		 }
+	}
+	public String GetValue(String key){
+		String value = props.getProperty (key);
+	 //   System.out.println(key+value);
+	    return value;
+	}
+
+	/**
+	 * 列出所有配置文件
+	 */
+	public void listall(){
+		
+		Enumeration en =	props.propertyNames();
+		while(en.hasMoreElements()){
+			String key = (String) en.nextElement();
+			 String Property = props.getProperty (key);
+             System.out.println("key:" + key+Property);
+            
+		}
+		
+	}
 }
